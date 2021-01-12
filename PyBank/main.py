748cc7acde91fb178566d53c32049d1a26fefe7b
Regine Strey <regine.strey@gmail.com>
2021-01-12 13:35:48 -0500

    Adding CSV files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,13 @@
 # Script for PyBank analysis
+
+import os
+import csv
+
+# PyBank\Resources\budget_data.csv
+filepath = os.path.join('PyBank', 'Resources', 'budget_data.csv').replace("\\","/")
+
+with open(filepath) as csvfile:
+
+    filereader = csv.reader(filepath, delimiter=',')
+
+    print(filepath)
